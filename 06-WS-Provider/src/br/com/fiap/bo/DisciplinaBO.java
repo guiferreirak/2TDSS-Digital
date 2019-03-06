bf5b1b2de9ffeb32ca3f1b82524302835504ce23
guiferreirak <gui.ferreirak@gmail.com>
2019-03-06 15:16:16 -0300

    Exemplos de Web Services

---
+++ 06-WS-Provider/src/br/com/fiap/bo/DisciplinaBO.java
@@ -0,0 +1,17 @@
+package br.com.fiap.bo;
+
+public class DisciplinaBO {
+	
+	public double calcularMedia(float am, float ps, float nac) {
+		return am*0.3 + nac*0.2 + ps*0.5;
+	}
+	
+	public boolean retidoPorFalta(int aulas, int faltas) {
+		return faltas/aulas > 0.25;
+	}
+	
+	public double calcularPS(float am, float nac) {
+		return (6 - (am*0.3 + nac * 0.2))*2;
+	}
+	
+}
---
+++ 07-WS-Loja-Provider/src/br/com/fiap/bo/ProdutoBO.java
@@ -0,0 +1,28 @@
+package br.com.fiap.bo;
+
+import org.apache.axis2.AxisFault;
+
+import br.com.fiap.to.ProdutoTO;
+
+public class ProdutoBO {
+	
+	//Regras de negócio	
+	public ProdutoTO consultarProduto(int codigo) throws Exception {
+		ProdutoTO pto = null;
+		switch (codigo) {
+		case 401:
+			pto = new ProdutoTO(401, 20, 2, "Camiseta branca");
+			break;
+		case 402:
+			pto = new ProdutoTO(402, 25, 1, "Camiseta preta");
+			break;
+		case 403:
+			pto = new ProdutoTO(403, 30, 3, "Camiseta rosa");
+			break;
+		default:
+			throw new AxisFault("Produto não encontrado");
+		}
+		return pto;
+	}
+	
+}
